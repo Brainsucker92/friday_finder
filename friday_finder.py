9f80c736768337f27540a4e188bb851bc0fac74f
Stefan <stscit21@hs-esslingen.de>
2021-09-18 23:06:26 +0200

    OOP is fun.

--- friday_finder.py
+++ friday_finder.py
@@ -2,19 +2,28 @@
 
 import datetime
 
-def get_next_friday(date):
-    days_to_friday = (4 - date.weekday()) % 7
-    return date + datetime.timedelta(days=days_to_friday)
 
-def find_next_friday_13th(date):
-    friday = get_next_friday(date)
-    while friday.day != 13:
-        friday += datetime.timedelta(weeks=1)
-    return friday
+class FridayFinder:
+    
+    @staticmethod
+    def get_days_to_friday(date):
+        return (4 - date.weekday()) % 7
+    
+    @staticmethod
+    def get_next_friday(date):
+        days_to_friday = FridayFinder.get_days_to_friday(date)
+        return date + datetime.timedelta(days=days_to_friday)
+    
+    @staticmethod
+    def find_next_friday_13th(date):
+        friday = FridayFinder.get_next_friday(date)
+        while friday.day != 13:
+            friday += datetime.timedelta(weeks=1)
+        return friday
 
 if __name__ == "__main__":
     today = datetime.date.today()
-    friday = get_next_friday(today)
-    friday_13th = find_next_friday_13th(today)
+    friday = FridayFinder.get_next_friday(today)
+    friday_13th = FridayFinder.find_next_friday_13th(today)
     print("Next friday will be at: " + str(friday))
     print("Next friday the 13th will be at: " + str(friday_13th))
