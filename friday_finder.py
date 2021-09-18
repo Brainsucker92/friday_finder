074d9204bb0b343c84f408d89296a7a3de78273a
Stefan <stscit21@hs-esslingen.de>
2021-09-18 23:17:53 +0200

    True OOP is even more fun.

--- friday_finder.py
+++ friday_finder.py
@@ -5,25 +5,27 @@ import datetime
 
 class FridayFinder:
     
-    @staticmethod
-    def get_days_to_friday(date):
-        return (4 - date.weekday()) % 7
+    def __init__(self, date):
+        self.__date = date
     
-    @staticmethod
-    def get_next_friday(date):
-        days_to_friday = FridayFinder.get_days_to_friday(date)
-        return date + datetime.timedelta(days=days_to_friday)
+    def get_days_to_friday(self):
+        weekday = self.__date.weekday()
+        return (4 - weekday) % 7
     
-    @staticmethod
-    def find_next_friday_13th(date):
-        friday = FridayFinder.get_next_friday(date)
+    def get_next_friday(self):
+        days_to_friday = self.get_days_to_friday()
+        return self.__date + datetime.timedelta(days=days_to_friday)
+    
+    def find_next_friday_13th(self):
+        friday = self.get_next_friday()
         while friday.day != 13:
             friday += datetime.timedelta(weeks=1)
         return friday
 
 if __name__ == "__main__":
     today = datetime.date.today()
-    friday = FridayFinder.get_next_friday(today)
-    friday_13th = FridayFinder.find_next_friday_13th(today)
+    finder = FridayFinder(today)
+    friday = finder.get_next_friday()
+    friday_13th = finder.find_next_friday_13th()
     print("Next friday will be at: " + str(friday))
     print("Next friday the 13th will be at: " + str(friday_13th))
